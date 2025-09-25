744860fe53500018a7df327dd1bb07b1b5a27f12
abyss_tutor <yejun6210@gmail.com>
2025-09-26 04:04:17 +0900

    [Chore] EnemyDetectionZone 범위 조절

--- src/main/java/org/newdawn/spaceinvaders/game_object/ingame/EnemyDetectionZone.java
+++ src/main/java/org/newdawn/spaceinvaders/game_object/ingame/EnemyDetectionZone.java
@@ -1,7 +1,6 @@
 package org.newdawn.spaceinvaders.game_object.ingame;
 
 import org.newdawn.spaceinvaders.fixed_point.FixedPointUtil;
-import org.newdawn.spaceinvaders.game_object.GameObject;
 import org.newdawn.spaceinvaders.game_object.GameObject2D;
 import org.newdawn.spaceinvaders.game_object.collision.Collider2D;
 import org.newdawn.spaceinvaders.game_object.collision.ICollider2DOwner;
@@ -9,11 +8,14 @@ import org.newdawn.spaceinvaders.game_object.ingame.bullet.EnemyBullet;
 import org.newdawn.spaceinvaders.game_object.ingame.enemy.Enemy;
 import org.newdawn.spaceinvaders.game_object.visual.SpriteRenderer;
 import org.newdawn.spaceinvaders.loop.GameLoop;
-import org.newdawn.spaceinvaders.loop.Loop;
 
 public class EnemyDetectionZone extends GameObject2D implements ICollider2DOwner{
-    private static final long detectRange = 30 << 16;
-    private static final int detectScore = 1;
+    private static final long detectRange = 18 << 16;
+    private static final int detectScore = 100;
+
+    private static final long dectectInterval = FixedPointUtil.ZERO_1;
+    private long detectElapsed = 0;
+    private boolean hasDetected = false;
 
     public EnemyDetectionZone(GameLoop loop, PlayerShip ship) {
         super(loop);
@@ -25,16 +27,34 @@ public class EnemyDetectionZone extends GameObject2D implements ICollider2DOwner
         collider2d.boundsPosY = -spriteRenderer.getSpritePivotY() - detectRange;
         collider2d.boundsWidth = (((long)spriteRenderer.getSpriteWidth()) << 16) + FixedPointUtil.mul(detectRange, 2 << 16);
         collider2d.boundsHeight = (((long)spriteRenderer.getSpriteHeight()) << 16) + FixedPointUtil.mul(detectRange, 2 << 16);
-        
-        setPos(ship.getPosX(), ship.getPosY());
+        collider2d.setDrawBounds(true);
+
+        addChild(collider2d);
+    }
+
+    @Override
+    protected void process(long deltaTime) {
+        super.process(deltaTime);
+
+        if (hasDetected){
+            if (detectElapsed >= dectectInterval){
+                hasDetected = false;
+                detectElapsed = 0;
+            }
+            else{
+                detectElapsed += deltaTime;
+            }
+        }
     }
 
     @Override
     public void collidedWith(ICollider2DOwner collider) {
-        if (collider instanceof Enemy || collider instanceof EnemyBullet){
-            System.out.println("안녕");
-            GameLoop gameLoop = (GameLoop)loop;
-            gameLoop.increaseScore(detectScore);
+        if (!hasDetected){
+            if (collider instanceof Enemy || collider instanceof EnemyBullet){
+                GameLoop gameLoop = (GameLoop)loop;
+                gameLoop.increaseScore(detectScore);
+                hasDetected = true;
+            }
         }
     }
     

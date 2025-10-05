1766e8f6237b53ca95e5e54b405fa41d6a9427f7
G-MEGA <scw973108@naver.com>
2025-10-05 23:58:33 +0900

    [Fix] Bomb 사용 시 리플레이나 롤백의 결과가 바뀌는 버그 픽스

--- src/main/java/event_bus/EventBus.java
+++ src/main/java/event_bus/EventBus.java
@@ -1,26 +1,25 @@
 package event_bus;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class EventBus {
-    Map<Class, Set<IEventBusSubscriber>> subscribers = new HashMap<>();
-    Map<Class, Set<IEventBusSubscriber>> forRegister = new HashMap<>();
-    Map<Class, Set<IEventBusSubscriber>> forUnregister = new HashMap<>();
+    // LinkedHashSet을 써야하는 이유)
+    //  그냥 HashSet은 순회 순서가 보장되질 않아서 롤백이나 리플레이시 의사 난수 호출 순서가 달라지므로 결과가 바뀜
+    Map<Class, LinkedHashSet<IEventBusSubscriber>> subscribers = new HashMap<>();
+    Map<Class, LinkedHashSet<IEventBusSubscriber>> forRegister = new HashMap<>();
+    Map<Class, LinkedHashSet<IEventBusSubscriber>> forUnregister = new HashMap<>();
 
     //Kryo 직렬화를 위한 매개변수 없는 생성자
     public EventBus() {}
 
     public void register(Class eventClass, IEventBusSubscriber subscriber){
-        if(!forRegister.containsKey(eventClass)) forRegister.put(eventClass, new HashSet<>());
+        if(!forRegister.containsKey(eventClass)) forRegister.put(eventClass, new LinkedHashSet<>());
 
         forRegister.get(eventClass).add(subscriber);
     }
 
     public void unregister(Class eventClass, IEventBusSubscriber subscriber){
-        if(!forUnregister.containsKey(eventClass)) forUnregister.put(eventClass, new HashSet<>());
+        if(!forUnregister.containsKey(eventClass)) forUnregister.put(eventClass, new LinkedHashSet<>());
 
         forUnregister.get(eventClass).add(subscriber);
     }
@@ -30,7 +29,7 @@ public class EventBus {
         for(Class eventClass:forRegister.keySet()){
             Set<IEventBusSubscriber> set = forRegister.get(eventClass);
             for(IEventBusSubscriber subscriber:set){
-                if(!subscribers.containsKey(eventClass)) subscribers.put(eventClass, new HashSet<>());
+                if(!subscribers.containsKey(eventClass)) subscribers.put(eventClass, new LinkedHashSet<>());
 
                 subscribers.get(eventClass).add(subscriber);
             }

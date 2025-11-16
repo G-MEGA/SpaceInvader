35d2ad50ceab8eceaf512f23291e56abbd15bf7b
G-MEGA <scw973108@naver.com>
2025-11-16 21:29:01 +0900

    [Refactor] SpriteRef 생성자 주석 밀어넣기

--- src/main/java/org/newdawn/spaceinvaders/sprite/SpriteRef.java
+++ src/main/java/org/newdawn/spaceinvaders/sprite/SpriteRef.java
@@ -3,8 +3,7 @@ package org.newdawn.spaceinvaders.sprite;
 public class SpriteRef {
     static public final String TEST_PASSIVE_SKILL_REF = "sprites/testPassiveSkill.png";
 
-    // Kryo 역직렬화를 위한 매개변수 없는 생성자
     public SpriteRef(){
-
+        // Kryo 역직렬화를 위한 매개변수 없는 생성자
     }
 }

b73d3586af42d9b90c17dcd2229d4d20c0719565
abyss_tutor <yejun6210@gmail.com>
2025-11-16 23:14:12 +0900

    [Refactor] ScreenEffectManager 생성하여 화면 효과 기능을 담당하게 만듦

--- src/main/java/org/newdawn/spaceinvaders/game_object/effect/BlankScreenEffect.java
+++ src/main/java/org/newdawn/spaceinvaders/game_object/effect/BlankScreenEffect.java
@@ -2,23 +2,25 @@ package org.newdawn.spaceinvaders.game_object.effect;
 
 import org.newdawn.spaceinvaders.game_object.GameObject2D;
 import org.newdawn.spaceinvaders.game_object.visual.SpriteRenderer;
-import org.newdawn.spaceinvaders.loop.Loop;
+import org.newdawn.spaceinvaders.loop.GameLoop;
 
 public class BlankScreenEffect extends GameObject2D {
     private long elapsed = 0;
     private long duration;
 
-    public BlankScreenEffect() {}
+    public BlankScreenEffect() {
+        // Kryo 역직렬화를 위한 매개변수 없는 생성자
+    }
 
-    public BlankScreenEffect(Loop loop, long duration) {
+    public BlankScreenEffect(GameLoop gameLoop, long duration) {
         this.duration = duration;
 
-        SpriteRenderer spriteRenderer = new SpriteRenderer(loop);
+        SpriteRenderer spriteRenderer = new SpriteRenderer(gameLoop);
         spriteRenderer.setSpriteRef("sprites/whiteBackground.png");
         spriteRenderer.setSortingLayer(100);
         addChild(spriteRenderer);
 
-        loop.addGameObject(this);
+        gameLoop.addGameObject(this);
     }
 
     @Override
